4172100fecf1842f18497d7b107d7b5a9561d965
MliliGenes <saad.elm.77@gmail.com>
2025-08-22 23:09:32 +0100

    why is it so hard to be alive

--- level_1/permutations/premutations.c
+++ level_1/permutations/premutations.c
@@ -33,17 +33,13 @@ int get_next_permutation(char *perm, int n)
     return (1);
 }
 
-void sort_char_arr(char *perm, int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (perm[j] > perm[j + 1])
-            {
-                char tmp = perm[j];
-                perm[j] = perm[j + 1];
-                perm[j + 1] = tmp;
+void sort_char_arr(char *str, int n){
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (str[j] > str[j + 1]) {
+                char temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
             }
         }
     }
@@ -62,8 +58,8 @@ int main(int ac, char *av[])
 
 
 
-    // sort_char_arr(perm, n);
+    sort_char_arr(perm, n);
     printf("%s\n", perm);
-    while (get_next_permutation(perm, n));
+    while (get_next_permutation(perm, n))
         printf("%s\n", perm);
 }
