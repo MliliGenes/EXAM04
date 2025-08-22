e09cc5ab5231a11b2860dbd1fba0123e9841e5d5
MliliGenes <saad.elm.77@gmail.com>
2025-08-23 00:23:54 +0100

    i am not finna burn out for a chance

--- level_1/permutations/premutations.c
+++ level_1/permutations/premutations.c
@@ -1,41 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
-int get_next_permutation(char *perm, int n)
-{
-    int i = n - 1;
-    while (i > 0 && perm[i - 1] >= perm[i])
-        i--;
-
-    if (i <= 0)
-        return (0);
-
-
-
-    int j = n;
-    while (j > i && perm[j - 1] <= perm[i - 1])
-        j--;
-    char tmp = perm[i - 1];
-    perm[i - 1] = perm[j - 1];
-    perm[j - 1] = tmp;
-
-
-
-    i++;
-    j = n;
-    while (i < j) 
-    {
-        tmp = perm[i - 1];
-        perm[i - 1] = perm[j - 1];
-        perm[j - 1] = tmp;
-        i++;
-        j--;
-    }
-    return (1);
-}
-
-void sort_char_arr(char *str, int n){
+// Simple bubble sort
+void bubbleSort(char *str, int n) {
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 0; j < n - 1; j++) {
             if (str[j] > str[j + 1]) {
                 char temp = str[j];
                 str[j] = str[j + 1];
@@ -45,21 +14,66 @@ void sort_char_arr(char *str, int n){
     }
 }
 
-int main(int ac, char *av[])
-{
-    if (ac == 1)
-        return (0);
+// Reverse part of string from index 'start' to end
+void reverse(char *str, int start, int length) {
+    int end = length - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
 
+// Generate next permutation in lexicographic order
+int nextPermutation(char *str, int length) {
+    // Step 1: Find rightmost character that is smaller than its next character
+    int i = length - 2;
+    while (i >= 0 && str[i] >= str[i + 1]) {
+        i--;
+    }
+    
+    // If no such character found, we're at the last permutation
+    if (i < 0) {
+        return 0;  // No more permutations
+    }
+    
+    // Step 2: Find rightmost character greater than str[i]
+    int j = length - 1;
+    while (str[j] <= str[i]) {
+        j--;
+    }
+    
+    // Step 3: Swap str[i] and str[j]
+    char temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+    
+    // Step 4: Reverse the suffix starting at str[i+1]
+    reverse(str, i + 1, length);
+    
+    return 1;  // Successfully generated next permutation
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("Usage: %s <string>\n", argv[0]);
+        return 1;
+    }
+    
+    char *str = argv[1];
+    int length = strlen(str);
         
-    int n = 0;
-    char *perm = av[1];
-    while (perm[n])
-        n++;
-
-
-
-    sort_char_arr(perm, n);
-    printf("%s\n", perm);
-    while (get_next_permutation(perm, n))
-        printf("%s\n", perm);
+    // Sort to get the first permutation
+    bubbleSort(str, length);
+    
+    printf("%s\n", str);  // Print first (sorted) permutation
+    
+    // Generate and print all remaining permutations
+    while (nextPermutation(str, length)) {
+        printf("%s\n", str);
+    }
+    
+    return 0;
 }
---
+++ level_1/permutations/tarshPremutations.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int get_next_permutation(char *perm, int n)
+{
+    int i = n - 1;
+    while (i > 0 && perm[i - 1] >= perm[i])
+        i--;
+
+    if (i <= 0)
+        return (0);
+
+
+
+    int j = n;
+    while (j > i && perm[j - 1] <= perm[i - 1])
+        j--;
+    char tmp = perm[i - 1];
+    perm[i - 1] = perm[j - 1];
+    perm[j - 1] = tmp;
+
+
+
+    i++;
+    j = n;
+    while (i < j) 
+    {
+        tmp = perm[i - 1];
+        perm[i - 1] = perm[j - 1];
+        perm[j - 1] = tmp;
+        i++;
+        j--;
+    }
+    return (1);
+}
+
+void sort_char_arr(char *str, int n){
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (str[j] > str[j + 1]) {
+                char temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main(int ac, char *av[])
+{
+    if (ac == 1)
+        return (0);
+
+        
+    int n = 0;
+    char *perm = av[1];
+    while (perm[n])
+        n++;
+
+
+
+    sort_char_arr(perm, n);
+    printf("%s\n", perm);
+    while (get_next_permutation(perm, n))
+        printf("%s\n", perm);
+}
